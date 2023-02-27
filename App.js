28b39983213a8b68855f802e4f983f49c579487b
Akshay Ramen <akramen10@gmail.com>
2023-02-27 17:32:12 -0600

    Added ability to delete goals

--- App.js
+++ App.js
@@ -1,45 +1,58 @@
-import {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList} from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-import GoalItem from './components/GoalItem'
-import GoalInput from './components/GoalInput'
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  
   const [courseGoals, setCourseGoals] = useState([]);
- 
-
-
 
   function addGoalHandler(enteredGoalText) {
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString()},
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
+  }
+
+  function deleteGoalHandler(id) {
+    setCourseGoals(currentCourseGoals =>  { return currentCourseGoals.filter((goal) => goal.id !== id );
+    });
   }
 
   return (
     <View style={styles.appContrainer}>
       <GoalInput onAddGoal={addGoalHandler} />
-     <View style={styles.goalsContainer} >
-     <FlatList data={courseGoals} renderItem={(itemData)=> {
-      return <GoalItem text={itemData.item.text}/>;
-     }}
-     keyExtract={(item, index) => {
-      return item.id; }}
-     />
-    </View>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler}  id={itemData.item.id}/>;
+          }}
+          keyExtract={(item, index) => {
+            return item.id;
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContrainer: {
-    flex:1,
+    flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
   goalsContainer: {
-    flex: 5
+    flex: 5,
   },
-
- });
+});
